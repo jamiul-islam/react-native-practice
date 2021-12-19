193e54745228eefc2acb8098d640ba3e3e907f7f
lucifer1112k <jamiulislamjami@hotmail.com>
2021-12-20 02:54:57 +0600

    image picker in app.js

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import React, { useEffect } from "react";
-import Screen from "./app/components/Screen";
+import React, { useEffect, useState } from "react";
+import { Button, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
-    const result = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    result.
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("you need to enable permission manually.");
   };
+
   useEffect(() => {
     requestPermission();
-  });
-  return <Screen></Screen>;
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select an image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
